4918bc6655bfb7ec018c3c9ba08b3f94e9aa8a7c
lolcol5 <36111239+lolcol5@users.noreply.github.com>
2018-02-06 17:31:29 -0500

    Add files via upload

---
+++ node/install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Node"
+echo "***Update***"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+
+echo "***Download Software***"
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ git libgmp-dev$
+sudo apt-get install git -y
+sudo mkdir $HOME/Azurite
+sudo mkdir $HOME/Azurite/DATA
+sudo mkdir $HOME/Azurite/TEMP
+cd $HOME/Azurite/DATA
+
+echo "***Download Miner***"
+sudo git clone https://github.com/magi-project/m-cpuminer-v2
+sudo git clone https://github.com/fireworm71/veriumMiner
+sudo git clone https://github.com/tpruvot/cpuminer-multi
+
+echo "***AUTOGEN***"
+echo "1/3"
+cd $HOME/Azurite/DATA/m-cpuminer-v2
+sudo ./autogen.sh
+echo "2/3"
+cd $HOME/Azurite/DATA/veriumMiner
+sudo ./autogen.sh
+echo "3/3"
+cd $HOME/Azurite/DATA/cpuminer-multi
+sudo ./autogen.sh
+echo "***AUTOGEN Complete***"
+
+echo "***CONFIGURE***"
+echo "1/3"
+cd $HOME/Azurite/DATA/m-cpuminer-v2
+sudo CFLAG="-O2 mfpu=neon-vfpv4" ./configure
+echo "2/3"
+cd $HOME/Azurite/DATA/veriumMiner
+sudo ./configure CFLAGS= --with-crypto --with-curl
+echo "3/3"
+cd $HOME/Azurite/DATA/cpuminer-multi
+sudo ./configure --disable-assembly CFLAGS="-Ofast -march=native" --with-crypto --with-curl
+echo "***CONFIGURE Complete***"
