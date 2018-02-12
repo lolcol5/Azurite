6fcd22f3e3ed6bf68d249e5d5792c4d00cb4c9f8
lolcol5 <36111239+lolcol5@users.noreply.github.com>
2018-02-11 19:26:56 -0500

    Add files via upload

--- server/install.sh
+++ server/install.sh
@@ -19,20 +19,20 @@ sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.old
 sudo cp /etc/vsftpd.conf /$HOME/a
 sudo rm /etc/vsftpd.conf
 sudo sed -i 's/anonymous_enable=No/anonymous_enable=Yes/g' $HOME/a
-sudo cp /$HOME/a /etc/vsftpd.conf
-sudo rm /$HOME/a
+sudo cp $HOME/a /etc/vsftpd.conf
+sudo rm $HOME/a
 
 echo "***Setting Node***"
 fab cmd:"sudo apt-get install git -y"
 fab cmd:"sudo git clone https://github.com/lolcol5/Azurite.git"
-fab cmd:"sudo chmod +x /$HOME/Azurite/Node/install.sh"
-fab cmd:"sudo /$HOME/Azurite/Node/./install.sh"
+fab cmd:"sudo chmod +x $HOME/Azurite/Node/install.sh"
+fab cmd:"sudo $HOME/Azurite/Node/./install.sh"
 
 echo "***Setting AzuriteSERVER***"
-sudo cp /$HOME/Azurite/Node/AzuriteCORE.sh /$HOME/AzuriteCORE.sh
-sudo chmod +x /$HOME/AzuriteCORE.sh
-sudo chmod +x /$HOME/updateAzuriteNODE.sh
-echo "#!/bin/bash" > /$HOME/updateAzuriteNODE.sh
-echo "#____updateAzuriteNODE____"  >> /$HOME/updateAzuriteNODE.sh
-echo "" >> /$HOME/updateAzuriteNODE.sh
+sudo cp $HOME/Azurite/Node/AzuriteCORE.sh $HOME/AzuriteCORE.sh
+sudo chmod +x $HOME/AzuriteCORE.sh
+sudo chmod +x $HOME/updateAzuriteNODE.sh
+echo "#!/bin/bash" > $HOME/updateAzuriteNODE.sh
+echo "#____updateAzuriteNODE____"  >> $HOME/updateAzuriteNODE.sh
+echo "" >> $HOME/updateAzuriteNODE.sh
 
