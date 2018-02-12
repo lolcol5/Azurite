4af27c3072fd78da92a1195a44fe4fd859176d27
lolcol5 <36111239+lolcol5@users.noreply.github.com>
2018-02-11 21:48:44 -0500

    Add files via upload

--- server/settingNODE.sh
+++ server/settingNODE.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo "settingNode vs 1.0"
+fab -f $HOME/Azurite/server/fabfile.py cmd:"sudo apt-get install git -y"
+fab -f $HOME/Azurite/server/fabfile.py cmd:"sudo git clone https://github.com/lolcol5/Azurite.git"
+fab -f $HOME/Azurite/server/fabfile.py cmd:"sudo chmod +x $NodeUser/Azurite/node/install.sh"
