8c5f3b7aff061699dd2ade89d02f94a51481146f
lolcol5 <36111239+lolcol5@users.noreply.github.com>
2018-02-12 21:48:20 -0500

    Delete makingNODE.sh

--- server/makingNODE.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "makingNode vs 1.0"
-
-#NodeUser== Username of the node
-NodeUser="home/fa"
-
-fab -f $HOME/Azurite/server/fabfile.py cmd:"sudo $NodeUser/Azurite/node/./install.sh"
